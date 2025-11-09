b7a0046da828cc53108963682132fe7ddb5b061e
cherkasovaa <alina.ch1202@gmail.com>
2025-11-09 16:38:14 +0300

    feat: add PUT /api/users/:id to update existing user

--- src/user-routes.ts
+++ src/user-routes.ts
@@ -82,3 +82,72 @@ router.post('/api/users', (req, res) => {
     }
   });
 });
+
+router.put('/api/users/:id', (req, res) => {
+  let body = '';
+
+  req.on('data', (chunk) => {
+    body += chunk.toString();
+  });
+
+  req.on('end', () => {
+    try {
+      const userId = req.url?.split('/')[3] || '';
+
+      if (!userId) {
+        return sendJson(res, 400, { message: 'User ID is required' });
+      }
+
+      if (!validate(userId)) {
+        return sendJson(res, 400, {
+          message: `User ID=${userId} is invalid (not uuid)`,
+        });
+      }
+
+      const userIdx = users.findIndex((user) => user.id === userId);
+
+      if (userIdx === -1) {
+        return sendJson(res, 404, {
+          message: `User with ID=${userId} not found`,
+        });
+      }
+
+      const userRequest: Partial<Omit<User, 'id'>> = JSON.parse(body);
+
+      const { username, age, hobbies } = userRequest;
+
+      const oldDataUser = users[userIdx];
+
+      if (!oldDataUser) {
+        return sendJson(res, 404, {
+          message: `User with ID=${userId} not found`,
+        });
+      }
+
+      const updatedUser = {
+        id: userId,
+        username: username || oldDataUser.username,
+        age: age || oldDataUser.age,
+        hobbies: hobbies || oldDataUser.hobbies,
+      };
+
+      if (
+        typeof updatedUser.username !== 'string' ||
+        typeof updatedUser.age !== 'number' ||
+        !Array.isArray(updatedUser.hobbies)
+      ) {
+        return sendJson(res, 400, {
+          message: `Invalid data types for fields. The username must be a string, age must be a number, hobbies must be an array of strings)`,
+        });
+      }
+
+      users[userIdx] = updatedUser;
+
+      sendJson(res, 200, updatedUser);
+    } catch (err) {
+      return sendJson(res, 400, {
+        message: `'Invalid request body: not a valid JSON`,
+      });
+    }
+  });
+});
